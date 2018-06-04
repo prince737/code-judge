5e7a08ab9be58fff144acb2363983793abbebd3a
prince737 <princedey51@gmail.com>
2018-06-04 21:02:12 +0530

    registeration page frontend completed

---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="icon" type="image/jpg" href="assets/icon.png" />
+	<title>Register</title>
+</head>
+<body>
+
+	<div class="container">
+	    <div class="login">
+	    	<p class="tag">REGISTER</p>
+	    	<form>
+	    		<div class="row">
+			        <div class="input-field col s12">
+			            <input id="email" type="text" class="validate">
+			            <label for="email">Team Id</label>
+			        </div>
+			    </div>
+			    <div class="row">
+			        <div class="input-field col s12">
+			            <input id="password" type="password" class="validate">
+			            <label for="password">Password</label>
+			        </div>
+			    </div>
+			    <input type="submit" name="login" value="GO">
+	    	</form>
+	    </div>
+	</div>
+
+	<div class="register"><i class="material-icons">add</i></div>
+
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</body>
+</html>
